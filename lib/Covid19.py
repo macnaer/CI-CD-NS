0c2691989e593be7119954bcfd3e76daf88615ad
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-09-13 17:47:14 +0300

    Start with database

--- lib/Covid19.py
+++ lib/Covid19.py
@@ -2,12 +2,22 @@ if __name__ == "__main__":
     pass
 
 import requests
-from lib.Settings import URL_COVID19
+from lib.Settings import URL_COVID19, HOSTNAME, USER, PASSWORD
+import mysql.connector
 
 
 class Covid19:
+
+    def __init__(self):
+        self.db = mysql.connector.connect(
+            host=HOSTNAME,
+            user=USER,
+            password=PASSWORD
+        )
+        print(self.db)
+
     def start(self):
-        print("Start getting currencies ...")
+
         self.__get_data(URL_COVID19)
 
     def __get_data(self, URL):
@@ -19,3 +29,19 @@ class Covid19:
                   country['TotalConfirmed'], country['NewDeaths'],
                   country['TotalDeaths'], country['NewRecovered'],
                   country['TotalRecovered'], country['Date'])
+
+        self.__save_data(countries)
+
+    def __save_data(self, countries):
+        cursor = self.db.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS covid19")
+        cursor.execute('USE covid19')
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS covid19_table (id INT AUTO_INCREMENT PRIMARY KEY, Country VARCHAR(255), CountryCode VARCHAR(255),  Slug VARCHAR(255), NewConfirmed INT(10), TotalConfirmed INT(10), NewDeaths INT(10), TotalDeaths INT(10), NewRecovered INT(10), TotalRecovered INT(10), Date DATETIME)")
+
+        # for country in countries['Countries']:
+        # sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+        # val = ("John", "Highway 21")
+        # cursor.execute(sql, val)
+
+        # self.db.commit()
--- lib/Settings.py
+++ lib/Settings.py
@@ -7,3 +7,8 @@ if __name__ == "__main__":
 FILENAME = "currency.txt"
 URL = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
 URL_COVID19 = "https://api.covid19api.com/summary"
+
+# Database configuration
+HOSTNAME = "35.202.180.26"
+USER = "root"
+PASSWORD = "password123"
